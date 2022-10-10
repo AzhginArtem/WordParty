6a59b4930437f4481f0eab80c7d3cbf38287e0a8
1XsteP1 <forroyalee2@gmail.com>
2022-10-10 18:04:22 +0300

    Add pages elements

--- wordparty/src/pages/Rooms.js
+++ wordparty/src/pages/Rooms.js
@@ -1,11 +1,151 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom'
+import newsImage from './../assets/contacts.svg'
 
 export default function Rooms() {
     return(
-        <main className="main">
-            <Link to="/lobby" className="main__btn">
-                Room
-            </Link>
+        <main>
+            <div className="news">
+                <h2 className="news__title">
+                    NEWS
+                </h2>
+                <div className="news__block">
+                    <img src={newsImage} alt="News" className="news__img"/>
+                    <h4 className="news__header">
+                        Text
+                    </h4>
+                    <p className="news__subheader">
+                        text for news
+                    </p>
+                </div>
+                <div className="news__block">
+                    <img src={newsImage} alt="News" className="news__img"/>
+                    <h4 className="news__header">
+                        Text
+                    </h4>
+                    <p className="news__subheader">
+                        text for news
+                    </p>
+                </div>
+            </div>
+            <div className="rooms">
+                <Link href='#'>
+                    <div className="rooms__block">
+                        <h2 className="rooms__title">
+                            Name
+                        </h2>
+                        <p className="rooms__subtitle">
+                            ID - Language
+                        </p>
+                        <p className="rooms__subtitle">
+                            Count of players
+                        </p>
+                    </div>
+                </Link>
+                <Link href='#'>
+                    <div className="rooms__block">
+                        <h2 className="rooms__title">
+                            Name
+                        </h2>
+                        <p className="rooms__subtitle">
+                            ID - Language
+                        </p>
+                        <p className="rooms__subtitle">
+                            Count of players
+                        </p>
+                    </div>
+                </Link>
+                <Link href='#'>
+                    <div className="rooms__block">
+                        <h2 className="rooms__title">
+                            Name
+                        </h2>
+                        <p className="rooms__subtitle">
+                            ID - Language
+                        </p>
+                        <p className="rooms__subtitle">
+                            Count of players
+                        </p>
+                    </div>
+                </Link>
+                <Link href='#'>
+                    <div className="rooms__block">
+                        <h2 className="rooms__title">
+                            Name
+                        </h2>
+                        <p className="rooms__subtitle">
+                            ID - Language
+                        </p>
+                        <p className="rooms__subtitle">
+                            Count of players
+                        </p>
+                    </div>
+                </Link>
+                <Link href='#'>
+                    <div className="rooms__block">
+                        <h2 className="rooms__title">
+                            Name
+                        </h2>
+                        <p className="rooms__subtitle">
+                            ID - Language
+                        </p>
+                        <p className="rooms__subtitle">
+                            Count of players
+                        </p>
+                    </div>
+                </Link>
+                <Link href='#'>
+                    <div className="rooms__block">
+                        <h2 className="rooms__title">
+                            Name
+                        </h2>
+                        <p className="rooms__subtitle">
+                            ID - Language
+                        </p>
+                        <p className="rooms__subtitle">
+                            Count of players
+                        </p>
+                    </div>
+                </Link>
+                <Link href='#'>
+                    <div className="rooms__block">
+                        <h2 className="rooms__title">
+                            Name
+                        </h2>
+                        <p className="rooms__subtitle">
+                            ID - Language
+                        </p>
+                        <p className="rooms__subtitle">
+                            Count of players
+                        </p>
+                    </div>
+                </Link>
+                <Link to={'lobby'}>
+                    <div className="rooms__block">
+                        <h2 className="rooms__title">
+                            Name
+                        </h2>
+                        <p className="rooms__subtitle">
+                            ID - Language
+                        </p>
+                        <p className="rooms__subtitle">
+                            Count of players
+                        </p>
+                    </div>
+                </Link>
+                <Link to={'lobby'}>
+                    <div className="rooms__block">
+                        <h2 className="rooms__title">
+                            Name
+                        </h2>
+                        <p className="rooms__subtitle">
+                            ID - Language
+                        </p>
+                        <p className="rooms__subtitle">
+                            Count of players
+                        </p>
+                    </div>
+                </Link>
+            </div>
         </main>
     )
 }
